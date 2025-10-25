cdd0d946fb28c6887552575c513b570efc3b1a6d
Alejandro Marcelo Arando Espejo <alearandoesp@gmail.com>
2025-10-24 20:24:51 -0400

    Se inicio un proyecto base de springboot para el microservicio de agenda

---
+++ backend/agendaservice/src/main/java/com/ucb/modulo9/agendaservice/AgendaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.modulo9.agendaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaserviceApplication.class, args);
+	}
+
+}
